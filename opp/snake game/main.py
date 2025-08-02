24af0b3a670f30d9aae70f0193efa72639a75514
Lakshay1221-apple <lakshayraj505@gmail.com>
2025-08-02 11:12:24 +0530

    DAY20: HALFWAY TO THE SNAKE GAME

---
+++ opp/snake game/main.py
@@ -0,0 +1,57 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+screen.update()
+
+snake = Snake()
+screen.listen()
+
+# Bind the keys to the snake's movement
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
